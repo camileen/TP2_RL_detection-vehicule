466d736382675951c0f87a58526db6741718388f
Camille Robinson <camille.robinson.026@gmail.com>
2025-04-10 08:58:13 +0200

    Add app-env.sh: create venv

---
+++ app-env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Create .venv directory
+mkdir -p /mnt/c/Users/byoub/Code/INSA-Lyon/IAT/TP2_RL_detection-vehicule/.venv
+cd /mnt/c/Users/byoub/Code/INSA-Lyon/IAT/TP2_RL_detection-vehicule/.venv
+python3 -m venv iat-tp2 # create a virtual environment named iat-tp2
